17f88825603d5e67904af50f90466a4bcff9da39
RubenV-Dev <r.will16@hotmail.com>
2019-11-18 14:18:27 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let arr = string.split(":");
+  let firstNum = parseInt(arr[0])
+  if (firstNum < 12) {
+    return "Good Morning";
+  }
+
+  else if (firstNum >= 12 && firstNum <= 17) {
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let greeting = document.querySelector("h1#greeting")
+  greeting.innerText = string;
+}
