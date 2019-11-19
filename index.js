ee36dc188b68caf71a13697f287c782f83f74b88
Javone <javosmith12@gmail.com>
2019-11-19 10:27:51 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str){
+  let time = parseInt(str)
+  
+  if(time < 12){
+    return "Good Morning";
+  } 
+  else if(time > 17){
+    return "Good Evening";
+  }
+  else
+    return "Good Afternoon";
+}
+
+function displayMessage(str){
+  let update = document.getElementById('greeting');
+  update.innerText = str;
+}
 /* Write your implementation of displayMessage() */
+
+/* Use like for like with parseInt when comparing integers
+remember to use curly braces and parenthesis()*/
